62d36af2c48719c69d1bef2cf4caf4a0b2c98380
r00nbun <119502014+r00nbun@users.noreply.github.com>
2023-05-11 14:32:24 +0300

    Add files via upload

---
+++ Task-5.py
@@ -0,0 +1,3 @@
+for x in range(1, 10+1):
+    if x!=4 and x!=7:
+        print (x)
---
+++ Task-7.py
@@ -0,0 +1,6 @@
+from Task_6 import summ
+
+a = int(input('Введите число a: '))
+b = int(input('Введите число b: '))
+
+print ('Сумма равна: ', summ(a, b))
